edc81f5f4dabe08bef8861800bf30ae31c03669d
DickersonAJ <adickerson10@stuy.edu>
2020-11-01 16:31:08 -0500

    set up Point.java and the tester as well as the get methods

---
+++ PointTester.java
@@ -0,0 +1,9 @@
+public class PointTester {
+
+  public static void main(String[] args) {
+    Point a = new Point(1, 2);
+    System.out.println(a.getX());
+    System.out.println(a.getY());
+  }
+
+}
