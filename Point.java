21cfe01d139d2c137fbe718f634a0a1ceace431c
DickersonAJ <adickerson10@stuy.edu>
2020-11-02 11:38:24 -0500

    added a null clause to equals

--- Point.java
+++ Point.java
@@ -24,7 +24,7 @@ public class Point {
   }
 
   public boolean equals(Point other) {
-    return (this.x == other.x && this.y == other.y);
+    return (other != null && this.x == other.x && this.y == other.y);
   }
 
 }
